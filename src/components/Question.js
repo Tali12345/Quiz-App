963b8a3419d783bf31d1df7cf226b45f23bb23a7
Tali <123tali12345@gmail.com>
2023-09-19 00:03:11 +0300

    Add simple table

---
+++ src/components/Question.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Question() {
+
+  return (
+    <div>
+        <table class="table table-hover table-sm">
+            <thead>
+                <tr class="table-primary">
+                    <th></th>
+                    <th>Qustion?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Answer 1</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Answer 2</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Answer 3</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Answer 4</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Question

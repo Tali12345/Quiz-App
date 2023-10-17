0269e6a2e53ca137981039749a0c72772a44dba1
Tali <123tali12345@gmail.com>
2023-10-17 17:22:26 +0300

    Add redux-persist local storage

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+import store, { persistor } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<h1>loading...</h1>} persistor= {persistor}>
+        <App/>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { quizSlice } from './features/quizSlice';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const createStore = () =>
-  configureStore({
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: storage
+  };
+
+  const persistedReducer = persistReducer(persistConfig, quizSlice.reducer);
+
+  const store = configureStore({
     reducer: {
-      quiz: quizSlice.reducer,
+      quiz: persistedReducer,
     },
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
   });
 
-export const store = createStore();
+export const persistor = persistStore(store);
+
+export default store;
